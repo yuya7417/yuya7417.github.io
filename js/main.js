9befbfe6a0fe46411770cd7c9d04be8ee9a686c0
yuya7417 <84897467+yuya7417@users.noreply.github.com>
2021-06-24 10:44:18 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,11 +5,8 @@
   const open = document.getElementById('open');
   const spmenu = document.querySelector('.sp-menu');
   const close = document.getElementById('close');
-  
-  const list1 = document.querySelector('.list1');
-  const list2 = document.querySelector('.list2');
-  const list3 = document.querySelector('.list3');
-  const list4 = document.querySelector('.list4');
+  const splist = document.querySelectorAll('.list');
+
 
   open.addEventListener('click', () => {
     spmenu.classList.add('show');
@@ -21,21 +18,11 @@
     open.classList.remove('hide');
   });
 
-  list1.addEventListener('click', () => {
-    spmenu.classList.remove('show');
-    open.classList.remove('hide');
-  });
-  list2.addEventListener('click', () => {
-    spmenu.classList.remove('show');
-    open.classList.remove('hide');
-  });
-  list3.addEventListener('click', () => {
-    spmenu.classList.remove('show');
-    open.classList.remove('hide');
-  });
-  list4.addEventListener('click', () => {
-    spmenu.classList.remove('show');
-    open.classList.remove('hide');
-  });
+  splist.forEach((list) => {
+    list.addEventListener('click', () => {
+      spmenu.classList.remove('show');
+      open.classList.remove('hide');
+    })
+  })
 
 }
